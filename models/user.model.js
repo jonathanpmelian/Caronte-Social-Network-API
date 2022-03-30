6811278b515790fdffd0c1c3961fcc9549587b0e
Jonathan Pulido <jonathanpmelian@gmail.com>
2022-03-30 22:09:40 +0100

    GENERAL: Server connection; USER: Login and Register

---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["User", "Admin"],
+    message: "{VALUE} is not supported",
+    default: "User",
+  },
+});
+
+const UserModel = mongoose.model("user", userSchema);
+module.exports = UserModel;
---
+++ routes/auth.router.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+const checkAuth = require("../utils/index");
+
+const { login, signup } = require("../controllers/auth.controller");
+
+router.post("/login", login);
+router.post("/signup", signup);
+router.get("/check", checkAuth, (req, res) => {
+  res.send("user valid and logged in");
+});
+
+module.exports = router;
