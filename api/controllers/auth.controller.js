eae6e7d4fd0f4fb9aa03e6f706d3150fc4f12158
Jonathan Pulido <jonathanpmelian@gmail.com>
2022-03-31 17:28:56 +0100

    GENERAL: Eslint added, folders grouped

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -25,7 +25,7 @@ async function login(req, res) {
     const user = await UserModel.findOne({ email: req.body.email });
     if (!user) return res.status(500).send("Email or password not valid");
 
-    bcrypt.compare(req.body.password, user.password, (err, result) => {
+    bcrypt.compare(req.body.password, user.password, (err) => {
       if (err) return res.status(500).send("Email or password not valid");
 
       const token = jwt.sign({ email: user.email }, process.env.SECRET);
