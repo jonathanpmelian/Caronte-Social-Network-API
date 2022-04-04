4215a439f60ab9cec9381efdb9d9bb8c4ab8ba1a
Alexis Rodriguez <k4nijo@gmail.com>
2022-04-04 22:45:42 +0100

    SUBSCRIPTION: CRUD Done

--- api/models/subscription.model.js
+++ api/models/subscription.model.js
@@ -2,8 +2,28 @@ const mongoose = require("mongoose");
 
 const subscriptionSchema = new mongoose.Schema({
   user: {
-    type: 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ["Anual", "Monthly", "Weekly"],
+  },
+  price: {
+    type: Number,
+    enum: [110, 10, 4],
+    required: true,
+  },
+  available: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
   }
 });
 
-module.exports = subscriptionSchema;
+const SubscriptionModel = mongoose.model("subscription", subscriptionSchema);
+module.exports = SubscriptionModel;
